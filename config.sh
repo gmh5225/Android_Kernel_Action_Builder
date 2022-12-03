37fedc594ce45f644b4f74cf70e28118e2de8979
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-12-03 12:21:26 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,10 @@ export REPO_ROOT=`pwd`
 
 # Paths
 export CLANG="${REPO_ROOT}/data/clang/bin/clang"
-export CROSS_COMPILE="${REPO_ROOT}/data/gcc/bin/aarch64-linux-android-"
+export HOSTCXX="${REPO_ROOT}/data/gcc/bin/aarch64-elf-g++" 
+export CROSS_COMPILE="${REPO_ROOT}/data/gcc/bin/aarch64-elf-"
+export CC_32="${REPO_ROOT}/data/gcc-arm/bin/arm-eabi-"
+export CC_COMPAT="${REPO_ROOT}/data/gcc-arm/bin/arm-eabi-gcc"
 export ANYKERNEL_DIR="${REPO_ROOT}/data/anykernel"
 export ANYKERNEL_IMAGE_DIR="${ANYKERNEL_DIR}"
 export KERNEL_DIR="${REPO_ROOT}/data/kernel"
@@ -19,11 +22,11 @@ if [ ! -z ${AKCI_CCACHE} ]; then
 fi
 
 # If not defined gives long compiler name
-export COMPILER_NAME="GCC"
+export COMPILER_NAME="CLANG"
 
 # Kernel config
-export DEFCONFIG="infinix_x573_defconfig"
-export KERNEL_NAME="Infinix-X573-Kernel"
+export DEFCONFIG="vendor/lisa_qgki_defconfig"
+export KERNEL_NAME="lisa-Kernel"
 
 export KBUILD_BUILD_USER="elf"
 export KBUILD_BUILD_HOST="buildstation"
