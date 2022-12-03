d0faefaf6eaed6d891fa6df4b9fa452ce4570ab8
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-12-03 13:02:26 -0800

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -5,12 +5,10 @@ echo "Syncing dependencies ..."
 mkdir "data"
 
 PIDS=""
-# ./sync.sh https://github.com/skyhuppa/kernel_land.git "data/kernel" "${REF}" &
-./sync.sh https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git "data/kernel" "${REF}" &
-# ./sync.sh https://github.com/Infinix-Hot-2/Mystery-Kernel-3.10.git "data/kernel" "${REF}" &
+ ./sync.sh https://github.com/skyhuppa/android_kernel_xiaomi_sm7325.git "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
-./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
-# ./sync.sh https://github.com/skyhuppa/gcc.git "data/gcc" &
+# ./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
+./sync.sh https://github.com/mvaisakh/gcc-arm.git "data/gcc" &
 PIDS="${PIDS} $!"
 ./sync.sh https://github.com/skyhuppa/proton-clang.git "data/clang" &
 PIDS="${PIDS} $!"
