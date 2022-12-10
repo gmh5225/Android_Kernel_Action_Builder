c506833e220e4ed85c42abc8de170f95ed9687a3
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-12-10 11:26:57 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -25,7 +25,7 @@ fi
 export COMPILER_NAME="CLANG"
 
 # Kernel config
-export DEFCONFIG="lisa_qgki_defconfig"
+# export DEFCONFIG="lisa_qgki_defconfig"
 export KERNEL_NAME="lisa-Kernel"
 
 export KBUILD_BUILD_USER="elf"
