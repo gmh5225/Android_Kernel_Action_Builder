c65e2133b852f0498850165e83b4a7f1c2c0daed
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-12-11 06:22:17 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,6 +11,8 @@ export CC_COMPAT="${REPO_ROOT}/data/gcc/bin/arm-eabi-gcc"
 export ANYKERNEL_DIR="${REPO_ROOT}/data/anykernel"
 export ANYKERNEL_IMAGE_DIR="${ANYKERNEL_DIR}"
 export KERNEL_DIR="${REPO_ROOT}/data/kernel"
+export DEFCONFIG_PATH="${KERNEL_DIR}/arch/arm64/configs/vendor/lisa-qgki_defconfig"
+
 
 # Define to enable ccache
 if [ ! -z ${AKCI_CCACHE} ]; then
@@ -25,7 +27,7 @@ fi
 # export COMPILER_NAME="CLANG"
 
 # Kernel config
-# export DEFCONFIG="lisa_qgki_defconfig"
+export DEFCONFIG="lisa_qgki_defconfig"
 export KERNEL_NAME="lisa-Kernel"
 
 export KBUILD_BUILD_USER="elf"
