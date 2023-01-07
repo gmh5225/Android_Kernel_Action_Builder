f3ef366a63180655b4ee12f14fa52d1da9a564f5
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-07 08:35:21 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,43 +4,33 @@ LABEL="$1"; REF="$2"
 . ./config.sh
 
 process_build () {
-   # Used by compiler
-   # export CC_FOR_BUILD=clang
+    # Used by compiler
+    export CC_FOR_BUILD=clang
     export LOCALVERSION="-${FULLNAME}"
-   # export DEFCONFIG_PATH="${KERNEL_DIR}/arch/arm64/configs/vendor/lisa-qgki_defconfig"
-   # Remove defconfig localversion to prevent overriding
-    sed -i -r "s/(CONFIG_LOCALVERSION=).*/\1/" "${KERNEL_DIR}/arch/arm64/configs/vendor/lisa-qgki_defconfig "
-    sed -i '13d;14d;15d;16d;17d' $KERNEL_DIR/scripts/depmod.sh
+    # Remove defconfig localversion to prevent overriding
+    sed -i -r "s/(CONFIG_LOCALVERSION=).*/\1/" "${KERNEL_DIR}/arch/arm64/configs/${DEFCONFIG}"
 
-  #      make O=out ARCH=arm64 vendor/lisa-qgki_defconfig 
-	make -j$(nproc --all) O=out                            \
-                              ARCH=arm64                       \
-                              LLVM=1                           \
-                              LLVM_IAS=1                       \
-                              AR=llvm-ar                       \
-                              NM=llvm-nm                       \
-                              LD=ld.lld                        \
-                              OBJCOPY=llvm-objcopy             \
-                              OBJDUMP=llvm-objdump             \
-                              STRIP=llvm-strip                 \
-                              CC=clang                         \
-                              CROSS_COMPILE=aarch64-linux-gnu- \
-			      KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1) \
-			  #    KBUILD_COMPILER_STRING="$(${CLANG} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" \
-          
-	  
+    make O=out ARCH=arm64 ${DEFCONFIG}
+    make -j$(nproc --all) O=out \
+        ARCH=arm64 \
+      #  CC="${CLANG}" \
+      #  CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE="${CROSS_COMPILE}" \
+   #     CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+        KBUILD_COMPILER_STRING="$(${CLANG} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" \
+    
     BUILD_SUCCESS=$?
     
     if [ ${BUILD_SUCCESS} -eq 0 ]; then
         mkdir -p "${ANYKERNEL_IMAGE_DIR}"
-        cp -f "${KERNEL_DIR}/out/arch/arm64/boot/Image" "${ANYKERNEL_IMAGE_DIR}/Image"
+        cp -f "${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb" "${ANYKERNEL_IMAGE_DIR}/Image.gz-dtb"
         cd "${ANYKERNEL_DIR}"
         zip -r9 "${REPO_ROOT}/${FULLNAME}.zip" * -x README
         cd -
     fi
     
     rm -rf "${KERNEL_DIR}/out"
-    rm "${ANYKERNEL_IMAGE_DIR}/Image"
+    rm "${ANYKERNEL_IMAGE_DIR}/Image.gz-dtb"
     return ${BUILD_SUCCESS}
 }
 
