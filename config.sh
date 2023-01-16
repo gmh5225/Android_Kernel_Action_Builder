29071016aa4af38b72d469f4b53201fb70884f9d
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-16 06:04:00 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ export REPO_ROOT=`pwd`
 # Paths
 export CLANG="${REPO_ROOT}/data/clang/bin/clang"
 # export HOSTCXX="${REPO_ROOT}/data/gcc64/bin/aarch64-elf-g++" 
-export CROSS_COMPILE="${REPO_ROOT}/data/gcc64/aarch64-linux-android-"
+export CROSS_COMPILE="${REPO_ROOT}/data/gcc/aarch64-linux-android-"
 # export CC_COMPAT="${REPO_ROOT}/data/gcc/bin/arm-eabi-gcc"
 export ANYKERNEL_DIR="${REPO_ROOT}/data/anykernel"
 export ANYKERNEL_IMAGE_DIR="${ANYKERNEL_DIR}"
