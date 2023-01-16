373e4f1241815cd285a03173c29534267cd4437e
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-16 05:16:59 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,9 +17,9 @@ process_build () {
          ARCH=arm64                                    \   
          CC="${CLANG}"                               \
          CLANG_TRIPLE=aarch64-linux-gnu-               \
-         CROSS_COMPILE="${CROSS_COMPILE}"              \           
-         CROSS_COMPILE_ARM32="${CROSS_COMPILE_ARM32}"   \
-         LD_LIBRARY_PATH="${REPO_ROOT}/data/clang/lib" \
+         CROSS_COMPILE=aarch64-linux-android-             \           
+         CROSS_COMPILE_ARM32=arm-linux-androideabi-   \
+       #  LD_LIBRARY_PATH="${REPO_ROOT}/data/clang/lib" \
          KBUILD_COMPILER_STRING="$(${clang} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" \
       
     BUILD_SUCCESS=$?
