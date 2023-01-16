381f41e72ddb73a242082f1b1fc8f98cdbeefce6
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-16 02:59:44 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,10 +4,10 @@ export REPO_ROOT=`pwd`
 
 # Paths
 export CLANG="${REPO_ROOT}/data/clang/bin/clang"
-# export HOSTCXX="${REPO_ROOT}/data/gcc64/bin/aarch64-elf-g++" 
-# export CROSS_COMPILE="${REPO_ROOT}/data/gcc64/bin/aarch64-elf-"
+export HOSTCXX="${REPO_ROOT}/data/gcc64/bin/aarch64-elf-g++" 
+export CROSS_COMPILE="${REPO_ROOT}/data/gcc64/bin/aarch64-elf-"
 export CROSS_COMPILE_COMPAT="${REPO_ROOT}/data/gcc/bin/arm-eabi-"
-# export CC_COMPAT="${REPO_ROOT}/data/gcc/bin/arm-eabi-gcc"
+export CC_COMPAT="${REPO_ROOT}/data/gcc/bin/arm-eabi-gcc"
 export ANYKERNEL_DIR="${REPO_ROOT}/data/anykernel"
 export ANYKERNEL_IMAGE_DIR="${ANYKERNEL_DIR}"
 export KERNEL_DIR="${REPO_ROOT}/data/kernel"
@@ -29,9 +29,9 @@ fi
 export LINKER=ld.lld
 
 # Kernel config
-# export DEFCONFIG="lisa-qgki_defconfig"
-export DEFCONFIG="trinket_defconfig"
-export KERNEL_NAME="trinket-Kernel"
+ export DEFCONFIG="lisa-qgki_defconfig"
+# export DEFCONFIG="trinket_defconfig"
+export KERNEL_NAME="lisa-Kernel"
 
 export KBUILD_BUILD_USER="elf"
 export KBUILD_BUILD_HOST="buildstation"
