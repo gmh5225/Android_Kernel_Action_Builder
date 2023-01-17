8a166228f593c5cbcc9e205264a7ad4d6f148da1
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-17 05:39:47 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -25,6 +25,6 @@ export COMPILER_NAME="CLANG"
 export DEFCONFIG="land_defconfig"
 export KERNEL_NAME="land-Kernel"
 
-export KBUILD_BUILD_USER="elf"
-export KBUILD_BUILD_HOST="buildstation"
-export KBUILD_BUILD_VERSION=1
+export KBUILD_BUILD_USER="skyhuppa"
+export KBUILD_BUILD_HOST="github_runner"
+export KBUILD_BUILD_VERSION=1.0
