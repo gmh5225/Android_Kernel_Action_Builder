4a9e34e1e6ffe71805d1fe123f4322285c092ac0
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-21 09:22:49 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ process_build () {
   
     make ARCH=arm64 vendor/${DEFCONFIG}
    # make O=out ARCH=arm64 ${DEFCONFIG}
-    make -j$(nproc --all)
+    make -j$(nproc --all)  \
          LLVM=1                                                      \
 	 LLVM_IAS=1                                                  \
 	 HOSTLD=ld.lld                                               \
