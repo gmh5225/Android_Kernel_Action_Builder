977155b20adfa09af61612a6e928d5545cdf284f
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-31 13:46:44 -0800

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -5,12 +5,12 @@ echo "Syncing dependencies ..."
 mkdir "data"
 
 PIDS=""
-# ./sync.sh https://github.com/skyhuppa/android_kernel_xiaomi_sm7325.git "data/kernel" "${REF}" &
-./sync.sh https://github.com/LineageOS/android_kernel_xiaomi_msm8998.git "data/kernel" "${REF}" &
+ ./sync.sh https://github.com/skyhuppa/android_kernel_xiaomi_sm7325.git "data/kernel" "${REF}" &
+# ./sync.sh https://github.com/LineageOS/android_kernel_xiaomi_msm8998.git "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
-./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
+#./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
 # ./sync.sh https://github.com/mvaisakh/gcc-arm64.git "data/gcc" &
-#./sync.sh https://github.com/mvaisakh/gcc-arm.git "data/gcc" &
+./sync.sh https://github.com/mvaisakh/gcc-arm.git "data/gcc" &
 PIDS="${PIDS} $!"
 ./sync.sh https://github.com/skyhuppa/proton-clang.git "data/clang" &
 PIDS="${PIDS} $!"
