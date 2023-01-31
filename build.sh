a9c02ec93ee145326351d4f3b563a81d7caf25b6
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-31 13:30:31 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,11 +17,11 @@ process_build () {
     make O=out ARCH=arm64 vendor/${DEFCONFIG}
    # make O=out ARCH=arm64 ${DEFCONFIG}
     make -j$(nproc --all) O=out 
-         ARCH=arm64                                    \   
-         CC="${CLANG}"                               \
-         CLANG_TRIPLE=aarch64-linux-gnu-               \
-         CROSS_COMPILE="${CROSS_COMPILE}"            \           
-         CROSS_COMPILE_ARM32=arm-linux-androideabi-   \
+         ARCH=arm64                                     \   
+         CC="${CLANG}"                                  \
+         CLANG_TRIPLE=aarch64-linux-gnu-                \
+         CROSS_COMPILE="${CROSS_COMPILE}"               \           
+         CROSS_COMPILE_ARM32=arm-linux-androideabi-     \
        #  LD_LIBRARY_PATH="${REPO_ROOT}/data/clang/lib" \
          KBUILD_COMPILER_STRING="$(${clang} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" \
       
