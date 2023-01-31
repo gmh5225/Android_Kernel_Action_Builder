747fcae226b9d9fd2863614cdb790de4d871b169
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-01-31 13:39:46 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ process_build () {
          CC="${CLANG}"                                  \
        # CLANG_TRIPLE=aarch64-linux-gnu-                \
          CROSS_COMPILE="${CROSS_COMPILE}"               \           
-         CROSS_COMPILE_ARM32=arm-linux-androideabi-     \
+         CROSS_COMPILE_ARM32="${CROSS_COMPILE_ARM32}"   \
        # LD_LIBRARY_PATH="${REPO_ROOT}/data/clang/lib"  \
          KBUILD_COMPILER_STRING="$(${clang} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" \
       
