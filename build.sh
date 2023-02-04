61965be835414925c30e111806d0e4b090730cba
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2023-02-04 14:12:46 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ process_build () {
     sed -i '13d;14d;15d;16d;17d' $KERNEL_DIR/scripts/depmod.sh
 
 # Clean source prior building. 1 is NO(default) | 0 is YES
-clean mrproper distclean
 INCREMENTAL='1'
 
 # Silence the compilation
@@ -20,7 +19,7 @@ INCREMENTAL='1'
     SILENCE='1'
    
    
-    make ARCH=arm64 vendor/${DEFCONFIG}
+    make O=out ARCH=arm64 vendor/${DEFCONFIG}
    # make O=out ARCH=arm64 ${DEFCONFIG}
     make -j$(nproc --all) O=out 
          ARCH=arm64                                     \   
