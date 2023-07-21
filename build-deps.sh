8562813c5b40e3f5d479ea5e81dfc692e50a913f
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-21 13:44:37 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -2,9 +2,7 @@
 
 . ./sync.sh
 
-echo "Syncing dependencies ..."
-
-mkdir "data"
+echo "Cloning dependencies ..."
 
 # ./sync.sh https://github.com/skyhuppa/kernel_land.git "data/kernel" "${REF}" &
 gut https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git -b Test_Build data/kernel
