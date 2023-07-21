3107c1655f299f6c03a7570b3c380fe572600e97
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-21 12:55:36 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -6,15 +6,15 @@ mkdir "data"
 
 PIDS=""
 # ./sync.sh https://github.com/skyhuppa/kernel_land.git "data/kernel" "${REF}" &
-./sync.sh https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git "data/kernel" &
+gut https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git "data/kernel" &
 # ./sync.sh https://github.com/skyhuppa/android_kernel_motorola_msm8937-2.git "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
-./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
+gut https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
 # ./sync.sh https://github.com/skyhuppa/gcc.git "data/gcc" &
 PIDS="${PIDS} $!"
-./sync.sh https://github.com/skyhuppa/proton-clang.git "data/clang" &
+gut https://github.com/skyhuppa/proton-clang.git "data/clang" &
 PIDS="${PIDS} $!"
-./sync.sh https://github.com/skyhuppa/AnyKernel3.git "data/anykernel" "master" &
+gut https://github.com/skyhuppa/AnyKernel3.git "data/anykernel" "master" &
 PIDS="${PIDS} $!"
 
 for p in $PIDS; do
