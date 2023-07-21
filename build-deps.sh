76ff7fe1004c180485baec120d97b3fd80597f13
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-21 13:25:07 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -6,21 +6,12 @@ echo "Syncing dependencies ..."
 
 mkdir "data"
 
-PIDS=""
 # ./sync.sh https://github.com/skyhuppa/kernel_land.git "data/kernel" "${REF}" &
-gut https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git "data/kernel" &
+gut https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git -b Test_Build data/kernel
 # ./sync.sh https://github.com/skyhuppa/android_kernel_motorola_msm8937-2.git "data/kernel" "${REF}" &
-PIDS="${PIDS} $!"
-gut https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
+gut https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-19.0 data/gcc
 # ./sync.sh https://github.com/skyhuppa/gcc.git "data/gcc" &
-PIDS="${PIDS} $!"
-gut https://github.com/skyhuppa/proton-clang.git "data/clang" &
-PIDS="${PIDS} $!"
-gut https://github.com/skyhuppa/AnyKernel3.git "data/anykernel" "master" &
-PIDS="${PIDS} $!"
-
-for p in $PIDS; do
-    wait $p || exit "$?"
-done
+gut https://github.com/skyhuppa/proton-clang.git -b master data/clang
+gut https://github.com/skyhuppa/AnyKernel3.git -b master "data/anykernel" maste
 
 echo "Done!"
