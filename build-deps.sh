1b9f4b7dac2a7d31dcf4b3041ab97ba2729dc7ef
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-21 14:01:52 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-. ./sync.sh
+echo "Syncing dependencies ..."
 
-echo "Cloning dependencies ..."
+mkdir "data"
 
+PIDS=""
 # ./sync.sh https://github.com/skyhuppa/kernel_land.git "data/kernel" "${REF}" &
-git https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git -b Test_Build data/kernel
-# ./sync.sh https://github.com/skyhuppa/android_kernel_motorola_msm8937-2.git "data/kernel" "${REF}" &
-git https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-19.0 data/gcc
+# ./sync.sh https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git "data/kernel" "${REF}" &
+ ./sync.sh https://github.com/skyhuppa/android_kernel_lenovo_msm8937.git "data/kernel" "${REF}" &
+PIDS="${PIDS} $!"
+./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
 # ./sync.sh https://github.com/skyhuppa/gcc.git "data/gcc" &
-git https://github.com/skyhuppa/proton-clang.git -b master data/clang
-git https://github.com/skyhuppa/AnyKernel3.git -b master "data/anykernel" master
+PIDS="${PIDS} $!"
+./sync.sh https://github.com/skyhuppa/proton-clang.git "data/clang" &
+PIDS="${PIDS} $!"
+./sync.sh https://github.com/skyhuppa/AnyKernel3.git "data/anykernel" "master" &
+PIDS="${PIDS} $!"
+
+for p in $PIDS; do
+    wait $p || exit "$?"
+done
 
 echo "Done!"
